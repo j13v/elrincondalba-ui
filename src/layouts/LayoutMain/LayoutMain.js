1958a5021c24d49eaa67ffa326dd4d10ac089326
rubeniskov <me@rubeniskov.com>
2019-02-03 23:25:54 +0100

    refactor: migrate HOC withStyles to Hook in LayoutMain

--- src/layouts/LayoutMain/LayoutMain.js
+++ src/layouts/LayoutMain/LayoutMain.js
@@ -1,6 +1,7 @@
 // Core
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
 // MuiComponents
 import Grid from '@material-ui/core/Grid';
 // Local Components
@@ -10,20 +11,25 @@ import withStyles from '../../styles/withStyles';
 // Styles
 import styles from './LayoutMain.style';
 
-export const LayoutMain = ({ classes, routes, children }) => (
-  <div className={classes.root}>
-    <Header className={classes.header} routes={routes} />
-    <Grid className={classes.container} container spacing={16}>{children}</Grid>
-    <Footer className={classes.footer} />
-  </div>
-);
+
+const useStyles = makeStyles(styles);
+
+export const LayoutMain = ({routes, children, ...restProps}) => {
+
+  const classes = useStyles(restProps);
+
+  return (
+    <div className={classes.root}>
+      <Header className={classes.header} routes={routes} />
+      <Grid className={classes.container} container spacing={16}>{children}</Grid>
+      <Footer className={classes.footer} />
+    </div>
+  );
+};
 
 
 LayoutMain.propTypes = {
-  classes: PropTypes.object.isRequired,
   routes: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-export default withStyles(styles, {
-  name: 'LayoutMain',
-})(LayoutMain);
+export default LayoutMain;
