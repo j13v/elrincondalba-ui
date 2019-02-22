b67bff762bae6f7bb51a9bf60b56dadc9bec2d00
Jorge Alonso López <jorge@local.local>
2019-02-22 19:04:34 +0100

    feat: add link social network

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
+import Link from '@material-ui/core/Link';
+import Typography from '../../routes/Home/components/ArticleGallery/ArticleGalleryTypography';
 import styles from './Footer.style';
+
 
 const useStyles = makeStyles(styles);
 
@@ -9,7 +12,20 @@ const Footer = (props) => {
   return (
     <div className={classes.root}>
       <div className={classes.main}>&nbsp;</div>
-      <div className={classes.ending}>&nbsp;</div>
+      <div className={classes.ending}>
+&nbsp;
+        <Typography variant="caption">
+          <Link href="https://www.instagram.com/elrincondalba/?hl=es" title="Instagram">
+                Instagram
+          </Link>
+          {' '}
+          <Link href="https://www.facebook.com/Elrincondealba2204/" title="Facebook">
+                Facebook
+          </Link>
+        </Typography>
+
+      </div>
+
     </div>
   );
 };
