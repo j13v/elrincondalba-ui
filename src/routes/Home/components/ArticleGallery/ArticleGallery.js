f55ab4b0215604a84618e84b1666e7061b5ed8ce
rubeniskov <me@rubeniskov.com>
2019-03-30 11:48:10 +0100

    refactor: change querystring format for filters in ArticleGallery hrefs

--- src/routes/Home/components/ArticleGallery/ArticleGallery.js
+++ src/routes/Home/components/ArticleGallery/ArticleGallery.js
@@ -93,28 +93,28 @@ function ArticleGallery(props) {
         'https://instagram.fmad10-1.fna.fbcdn.net/vp/e654fcba4edf0afd5ef465b1beb2fc53/5D215242/t51.2885-15/e35/49759350_2210075629230653_7917598251271262349_n.jpg?_nc_ht=instagram.fmad10-1.fna.fbcdn.net',
       title: 'Zapatos',
       width: '40%',
-      href: 'catalogo?categorias=[zapatos]',
+      href: 'catalogo?categorias=zapatos',
     },
     {
       url:
         'https://instagram.fmad10-1.fna.fbcdn.net/vp/430a62764d1d2e42c3966161fa0bc01a/5D0C692A/t51.2885-15/e35/51518238_2054116344891416_2525790445819923623_n.jpg?_nc_ht=instagram.fmad10-1.fna.fbcdn.net',
       title: 'Conjuntos',
       width: '20%',
-      href: 'catalogo?categorias=[conjuntos]',
+      href: 'catalogo?categorias=conjuntos',
     },
     {
       url:
         'https://instagram.fmad10-1.fna.fbcdn.net/vp/47a4502414beac904f5efe56c62091bb/5D0B253A/t51.2885-15/e35/46848678_842281679441984_3594095275529921691_n.jpg?_nc_ht=instagram.fmad10-1.fna.fbcdn.net',
       title: 'Chaquetas',
       width: '40%',
-      href: 'catalogo?categorias=[chaquetas]',
+      href: 'catalogo?categorias=chaquetas',
     },
     {
       url:
         'https://instagram.fmad10-1.fna.fbcdn.net/vp/29ba73d37424f8be6dc7eff7c6c403d1/5D1F47C7/t51.2885-15/sh0.08/e35/p750x750/44626715_356236835149483_3407600073304698707_n.jpg?_nc_ht=instagram.fmad10-1.fna.fbcdn.net',
       title: 'Blusas',
       width: '38%',
-      href: 'catalogo?categorias=[blusas]',
+      href: 'catalogo?categorias=blusas',
     },
     {
       url:
@@ -128,7 +128,7 @@ function ArticleGallery(props) {
         'https://instagram.fmad10-1.fna.fbcdn.net/vp/e6649238726bbb218eddafc31f527fd0/5D245C85/t51.2885-15/e35/37906449_471765419901706_2684274484629733376_n.jpg?_nc_ht=instagram.fmad10-1.fna.fbcdn.net',
       title: 'Complementos',
       width: '24%',
-      href: 'catalogo?categorias=[complementos]',
+      href: 'catalogo?categorias=complementos',
     },
     {
       url:
@@ -149,7 +149,7 @@ function ArticleGallery(props) {
         'https://instagram.fmad10-1.fna.fbcdn.net/vp/f13e2badf38f8d2ad610bc449180cccd/5CF599D3/t51.2885-15/e35/25025175_2031110043573556_3126835920628613120_n.jpg?_nc_ht=instagram.fmad10-1.fna.fbcdn.net',
       title: 'Jerseys',
       width: '40%',
-      href: 'catalogo?categorias=[jerseys]',
+      href: 'catalogo?categorias=jerseys',
     },
   ];
 
